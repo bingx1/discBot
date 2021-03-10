cade3c2900a2d614f757ff5a5376f0245eb837e9
bing xu <bingxu.au@gmail.com>
2021-03-10 12:24:31 +1100

    fixed db connection

--- connection.py
+++ connection.py
@@ -3,7 +3,8 @@ from mongoengine import connect
 
 class Connection:
     def __enter__(self):
-        self.conn = connect(db="items", host="localhost", port=27017)
+        self.conn = connect(db="items", host="mongodb")
+        print("Connected to mongodb")
         return self.conn
 
     def __exit__(self, exc_type, exc_val, exc_tb):
