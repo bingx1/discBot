112ec84418c88195139e07de2e22c3ade8c26275
bing xu <bingxu.au@gmail.com>
2021-03-13 00:25:21 +1100

    finalised home page design

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 @app.route('/index')
 @app.route('/')
 def index():
-    items = [{'name': 'Rogue Ohio Deadlift Bar - Black Zinc', 'price': 550, 'stock': False, 'url': 'https://www.rogueaustralia.com.au/rogue-ohio-deadlift-bar-black-zinc-au', 'img_url': 'https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Mens%2020KG%20Barbells/AU-RA0963-BLBR/AU-RA0963-BLBR-h_t4opya.png'},
-             {'name': 'Rogue 20KG Ohio Power Bar - Stainless Steel', 'price': 680, 'stock': True, 'url': 'https://www.rogueaustralia.com.au/rogue-20-kg-ohio-power-bar-stainless-steel-au', 'img_url':'https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Mens%2020KG%20Barbells/RA0692-SSDC/20kg-ohio-power-bar-header-1_1_IPF_qbhfyb.jpg'}]
+    items = [{'name': 'Rogue Ohio Deadlift Bar - Black Zinc', 'manufacturer':'Rogue', 'price': 550, 'stock': False, 'url': 'https://www.rogueaustralia.com.au/rogue-ohio-deadlift-bar-black-zinc-au', 'img_url': 'https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Mens%2020KG%20Barbells/AU-RA0963-BLBR/AU-RA0963-BLBR-h_t4opya.png'},
+             {'name': 'Rogue 20KG Ohio Power Bar - Stainless Steel', 'manufacturer':'Rogue','price': 680, 'stock': True, 'url': 'https://www.rogueaustralia.com.au/rogue-20-kg-ohio-power-bar-stainless-steel-au', 'img_url':'https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Mens%2020KG%20Barbells/RA0692-SSDC/20kg-ohio-power-bar-header-1_1_IPF_qbhfyb.jpg'}]
 
     return render_template('index.html', title='Home', items=items)
 
