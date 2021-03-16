f69f46822a0b5c85f170a4399ec3db95348b93b3
bing xu <bingxu.au@gmail.com>
2021-03-16 23:07:56 +1100

    added sample payloads for mongodb

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, request, render_template
 from util.db_handler import DbHandler
 app = Flask(__name__)
-DbHandler = DbHandler()
+DbHandler = DbHandler() 
+
 items = [{"name" : "Rogue 45LB Ohio Power Bar - Bare Steel", 'manufacturer':'Rogue', "price" : 450, "Stock" : False, "url" : "https://www.rogueaustralia.com.au/rogue-ohio-power-bar-45-lb-bare-steel-au", "img_url" : "https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Mens%2020KG%20Barbells/AU-RA0586-RWRW/AU-RA0586-RWRW-h_izz6x3.png" },
          {"name" : "Rogue TB-2 Trap Bar", 'manufacturer':'Rogue', "price" : 618, "Stock" : True, "url" : "https://www.rogueaustralia.com.au/rogue-tb-2-trap-bar-au", "img_url" : "https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Weightlifting%20Bars%20and%20Plates/Barbells/Specialty%20Barbells/AU-RA0711/AU-RA0711-h_zvzjbv.png"},
          {"name" : "Rogue Combo Rack", 'manufacturer':'Rogue', "price" : 4420, "Stock" : False, "url" : "https://www.rogueaustralia.com.au/rogue-combo-rack-au", "img_url" : "https://assets.roguefitness.com/f_auto,q_auto,h_400,b_rgb:f8f8f8/catalog/Rigs%20and%20Racks/Squat%20Stands/Monster%20Lite%20Squat%20Stands/AU-RA1736-BLACK-MG/AU-RA1736-BLACK-MG-h_pyflph.png" }, 
@@ -11,11 +12,11 @@ items = [{"name" : "Rogue 45LB Ohio Power Bar - Bare Steel", 'manufacturer':'Rog
          {'name': 'ATX Buffalo Olympic Bar', 'manufacturer':'ATX', 'price': 560, 'stock': False, 'url':'https://samsfitness.com.au/barbells/specialty-olympic-bars/buffalo-squat-bar', 'img_url':'https://samsfitness.com.au/image/cache/catalog/ATX-LH-BUFFALO/atx-buffalo-bar-800x800.jpg'}]
 
 changes = [{"item_name" : "Rogue 45LB Ohio Power Bar - Bare Steel", 'restock': True, 'time': '7:15PM', 'date': '13/03/2021'},
-           {"item_name" : "Rogue 45LB Ohio Power Bar - Bare Steel", 'restock': False, 'time': '8:33AM', 'date': '15/03/2021'},
+           {"item_name" : "Rogue Ohio Deadlift Bar - Black Zinc", 'restock': False, 'time': '8:33AM', 'date': '15/03/2021'},
            {"item_name" : "Rogue 20KG Ohio Power Bar - Stainless Steel", 'restock': True, 'time': '4:45PM', 'date': '12/03/2021'},
            {"item_name" : "ATX Buffalo Olympic Bar", 'restock': True, 'time': '9:14AM', 'date': '11/03/2021'},
            {"item_name" : "Rogue Combo Rack", 'restock': False, 'time': '11:33AM', 'date': '10/03/2021'}]
-
+ 
 @app.route('/index')
 @app.route('/')
 def index():
@@ -48,6 +49,10 @@ def parse_query(query, items):
             if q in item['name'].lower().split():
                 result.append(item)
     return result
+
+# @app.route('/items')
+    # req = request.form
+
 
 @app.route('/dashboard')
 def dashboard():
