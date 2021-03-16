5125ae4be51f4b686a0eca388fda667a2cccb946
bing xu <bingxu.au@gmail.com>
2021-03-16 23:30:54 +1100

    added date_added field to items and updated Changes schema

--- util/models.py
+++ util/models.py
@@ -7,6 +7,7 @@ class Item(mongoengine.Document):
     stock = mongoengine.BooleanField(required=True)
     url = mongoengine.URLField(required=True)
     lastStocked = mongoengine.DateTimeField()
+    date_added =mongoengine.DateTimeField()
     img_url = mongoengine.URLField(required=True)
     meta = {'collection': 'items'}
 
@@ -18,7 +19,8 @@ class Item(mongoengine.Document):
         return "{}\n[${}]({})\n{}\n < Last in stock: {} >\n".format(
             self.name, self.price, self.url, stock_msg, self.lastStocked)
 
+# {"item_name" : "Rogue 45LB Ohio Power Bar - Bare Steel", 'restock': true, 'time': '7:15PM', 'date': '13/03/2021'}
 class Change(mongoengine.Document):
-    name = mongoengine.StringField(required=True)
-    restock = mongoengine.URLField(required=True)
-    other = mongoengine.BooleanField()
+    item_name = mongoengine.StringField(required=True)
+    restock = mongoengine.BooleanField(required=True)
+    timestamp = mongoengine.DateTimeField(required=True)
